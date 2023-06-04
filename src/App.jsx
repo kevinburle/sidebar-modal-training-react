38a4149ab2f51260fb9f18d7a0236500676bb4f9
Kévin Burle <kevinburle.be@gmail.com>
2023-06-04 10:51:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Sidebar />
+      <Modal />
+    </>
+  );
+};
+export default App;
